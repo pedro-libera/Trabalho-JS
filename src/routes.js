579e18f5f5c82cd63c0526e0804c4bfcbacfd393
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-09-21 18:16:11 -0300

    Acertando questão das promises

--- src/routes.js
+++ src/routes.js
@@ -4,36 +4,58 @@ const axios = require("axios");
 
 routes.get("/starwars/filmes/:numero", async (req, res) => {
 
-
     const response = await axios.get(`https://swapi.dev/api/films/${req.params.numero}`);
 
     const arrayUrlPersonagens = response.data.characters;
 
-    async function nomesPersonagens() {
-       /*  let arrayNomes = []; */
+    let arrayNomes = []
 
-        const arrayNomes = await arrayUrlPersonagens.map(async (url) => {
+    for(let i = 0; i < arrayUrlPersonagens.length; i++) {
+        let nome =  await axios.get(arrayUrlPersonagens[i]);
+        console.log(nome.data.name)        
+        arrayNomes.push(nome.data.name);
+    }
 
-            const personagem = await axios.get(url);
+    console.log(arrayUrlPersonagens)
+    console.log(arrayNomes)
 
-            return personagem.data.name ;
-        });
+    // const createArrayNomes = arrayUrl => new Promise((resolve, reject) => {
+    //     try {
+    //         const arrayNomesResponse = arrayUrl.map( async url => await axios.get(url))
+    //         resolve(arrayNomesResponse)
+    //     } catch (error) {
+    //         reject(error)
+    //     }
+    // })
 
-        console.log(arrayNomes)
+    // const arrayNomes = await createArrayNomes(arrayUrlPersonagens);
 
-        return  new Promise((resolve, reject)=>{
-            if(arrayNomes){
-                resolve(arrayNomes)
-            }else{
-                reject("deu errado")
-            }
-        });
-    };
 
-    const resposta = await nomesPersonagens();
+    // async function nomesPersonagens() {
+    //    /*  let arrayNomes = []; */
+
+    //     const arrayNomes = await arrayUrlPersonagens.map(async (url) => {
+
+    //         const personagem = await axios.get(url);
+
+    //         return personagem.data.name ;
+    //     });
+
+    //     console.log(arrayNomes)
+
+    //     return  new Promise((resolve, reject)=>{
+    //         if(arrayNomes){
+    //             resolve(arrayNomes)
+    //         }else{
+    //             reject("deu errado")
+    //         }
+    //     });
+    // };
+
+    // const resposta = await nomesPersonagens();
 
     res.json({
-        "data": resposta
+        "data": arrayNomes
     });
 });
 
