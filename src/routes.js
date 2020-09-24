092d87420fad9092b677a9247c72379ca0030980
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-09-24 14:46:47 -0300

    Ajuste de promises

--- src/routes.js
+++ src/routes.js
@@ -14,45 +14,7 @@ routes.get("/starwars/filmes/:numero", async (req, res) => {
         let nome =  await axios.get(arrayUrlPersonagens[i]);
         console.log(nome.data.name)        
         arrayNomes.push(nome.data.name);
-    }
-
-    console.log(arrayUrlPersonagens)
-    console.log(arrayNomes)
-
-    // const createArrayNomes = arrayUrl => new Promise((resolve, reject) => {
-    //     try {
-    //         const arrayNomesResponse = arrayUrl.map( async url => await axios.get(url))
-    //         resolve(arrayNomesResponse)
-    //     } catch (error) {
-    //         reject(error)
-    //     }
-    // })
-
-    // const arrayNomes = await createArrayNomes(arrayUrlPersonagens);
-
-
-    // async function nomesPersonagens() {
-    //    /*  let arrayNomes = []; */
-
-    //     const arrayNomes = await arrayUrlPersonagens.map(async (url) => {
-
-    //         const personagem = await axios.get(url);
-
-    //         return personagem.data.name ;
-    //     });
-
-    //     console.log(arrayNomes)
-
-    //     return  new Promise((resolve, reject)=>{
-    //         if(arrayNomes){
-    //             resolve(arrayNomes)
-    //         }else{
-    //             reject("deu errado")
-    //         }
-    //     });
-    // };
-
-    // const resposta = await nomesPersonagens();
+    }  
 
     res.json({
         "data": arrayNomes
